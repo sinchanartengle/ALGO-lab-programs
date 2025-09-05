d68f0407981e2359a26f2d684c93520e7fa60d2a
sinchanartengle <sinchanartengle2005@gmail.com>
2025-09-05 19:12:50 +0530

    Update 1. Knapsack01.c

--- 1. Knapsack01.c
+++ 1. Knapsack01.c
@@ -1,93 +1,97 @@
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <time.h>
-// Function to read input into an array
-// void read(int arr[],int n);
+#include<iostream>
+using namespace std;
+#define MAX 10
 
-// Recursive 0/1 Knapsack function
-int knapsack(int ind,int W,int wt[],int val[]);
+int knap[MAX][MAX];
 
-int main() {
-    srand(time(0));
-    int W=0,n=10;
-    FILE *f;
-    clock_t t;
-    f=fopen("KnapAvg.txt","a");
-    int *val=NULL,*wt=NULL;
-    while(n<=130){
-        //Allocating Space Dynamically
-        val=(int*)malloc(sizeof(int)*n);
-        wt=(int*)malloc(sizeof(int)*n);
-        //Average Case
-        for(int i=0;i<n;i++){
-            val[i]=rand()%n;
-            wt[i]=rand()%n;
-        }   
-        //Knapsack Size
-        W=n;
-        t=clock();
-        knapsack(n-1,W,wt,val);
-        t=clock()-t;
-        fprintf(f,"%d\t%f\n",n,(float)t/CLOCKS_PER_SEC);
-        printf("%d\t%f\n",n,(float)t/CLOCKS_PER_SEC);
-        free(val);
-        free(wt);
-        n+=20;
-    }
-    fclose(f);
-    return 0;
-}
-int knapsack(int ind,int W,int wt[],int val[]){
-    //Base Case
-    if(ind==0){
-        if(wt[0]<=W)
-            return val[0];
-        return 0;
-    }
-    // Do not take current item
-    int notTake=0+knapsack(ind-1,W,wt,val);
-    int take=INT_MIN;
-    //Take current item (if it fits)
-    if(wt[ind]<=W){
-        take=val[ind]+knapsack(ind-1,W-wt[ind],wt,val);
-    }
-    // Return the maximum of both choices
-    return take>notTake?take:notTake;
+int max(int a,int b){
+   return (a>b) ? a : b ;
 }
 
+int knapsack(int items, int capacity, int profit[], int weight[]){
+    int i, j;
+    for (i=1; i<=items; i++){
+        for (j=1; j <=capacity; j++){
+            if (j < weight[i]) {
+                knap[i][j] = knap[i - 1][j];
+            } else {
+                knap[i][j] = max(knap[i - 1][j], profit[i] + knap[i - 1][j - weight[i]]);
+            }
+        }
+    }
+   return knap[items][capacity];
+}
 /*
-Space Complexity = O(n) (recursive stack space)
-Time Complexity = O(2ⁿ)
-https://media.geeksforgeeks.org/wp-content/uploads/20250312122618748210/Recursion-Tree-for-01-KnapSack-2.webp
+int knapsack(int items, int capacity, int profit[], int weight[]){
+    int i,j;
+     for (i = 1; i <= items; i++){
+        for (j = 1; j <= capacity; j++){
+            knap[i][j] = (j < weight[i]) ? knap[i - 1][j] : max(knap[i - 1][j], profit[i] + knap[i - 1][j - weight[i]]);
+        }
+   }
+    return knap[items][capacity];
+}
 */
 
-/*
-void read(int arr[],int n){
-    for(int i=0;i<n;i++)
-        scanf("%d",&arr[i]);
+int main(){
+    int i,j,n,items,capacity,profit[MAX],weight[MAX],optimum_val;
+    cout << "Enter No. of objects: ";
+    cin >>items;
+    cout << "Enter the Weights: ";
+    for (i=1;i<=items;i++)
+        cin>>weight[i];
+    cout << "Enter the Profits: ";
+    for (i=1;i<=items;i++)
+        cin>>profit[i];
+    cout << "Enter the Knapsack Capacity: ";
+    cin >> capacity;
+    optimum_val=knapsack(items,capacity,profit,weight);
+    cout<<"Profit Table\n";
+    for(i=0;i<=items;i++){
+      for(j=0;j<=capacity;j++){
+        cout<<knap[i][j]<<"\t";
+      }
+      cout<<"\n";
+    }
+    cout<<"\nThe Maximum Profit is : "<<optimum_val;
 }
 
-int main() {
-    int W,n;
-    // Reading the total capacity of the knapsack
-    printf("\nEnter the weight of the bag: ");
-    scanf("%d",&W);
-     // Reading the number of items
-    printf("\nEnter the number of elements in array: ");
-    scanf("%d",&n);
-    //Allocating Space Dynamically
-    int* val=(int*)malloc(sizeof(int)*n);
-    int* wt=(int*)malloc(sizeof(int)*n);
-    // Reading weights and values
-    printf("\nEnter the elements of Weight Array: ");
-    read(wt,n);
-    printf("\nEnter the elements of corresponding Values(Profit) Array: ");
-    read(val,n);
-    printf("The Max. Profit is: %d",knapsack(n-1,W,wt,val));
-    // Freeing dynamically allocated memory
-    free(val);
-    free(wt);
-    return 0;
+/* 
+#include <iostream>
+using namespace std;
+
+int p[50], w[50], n;
+
+int knapsack(int i, int m)
+{
+    if (i == n)
+        return (w[n] > m) ? 0 : p[n];
+    if (w[i] > m)
+        return knapsack(i + 1, m);
+    return max(knapsack(i + 1, m), knapsack(i + 1, m - w[i]) + p[i]);
+}
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int m, i, optsoln;
+    cout << "Enter no. of objects: ";
+    cin >> n;
+    cout << "\nEnter the weights:\n";
+    for (i = 1; i <= n; i++)
+        cin >> w[i];
+    cout << "\nEnter the profits:\n";
+    for (i = 1; i <= n; i++)
+        cin >> p[i];
+    cout << "\nEnter the knapsack capacity:";
+    cin >> m;
+    optsoln = knapsack(1, m);
+    cout << "\nThe optimal soluntion is:" << optsoln;
 }
 */

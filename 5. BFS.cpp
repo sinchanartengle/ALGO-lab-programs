ae9343c1384f3be97dd09979568fdfe7292a5f11
sinchanartengle <sinchanartengle2005@gmail.com>
2025-09-15 19:19:17 +0530

    5. BFS.cpp

--- 5. BFS.cpp
+++ 5. BFS.cpp
@@ -3,40 +3,43 @@
 method. 
 */
 #include <iostream>
-#include <queue>
-#include <vector>
 using namespace std;
 
-// Function to perform BFS
-void bfs(int start,int V,vector<int>& ans,vector<vector<int>>& adj);
-
-int main(){
-    vector<vector<int>> adj = {{1,2}, {0,2,3}, {0,4}, {1,4}, {2,3}};
-    vector<int> ans; 
-    int V=adj.size();
-    int start=0;
-    bfs(start,V,ans,adj);
-    for(auto i:ans) {
-        cout<<i<<" ";
+void bfs(int cost[20][20],int source,int visited[20],int n){
+    int queue[20], u, v;
+    int f= 0;
+    int r= -1;
+    queue[++r]=source;
+    while (f<=r){
+        u=queue[f++];
+        for (v=1;v<=n;v++){
+            if (cost[u][v] == 1&&visited[v] == 0){
+                queue[++r]=v;
+                visited[v]=1;
+            }
+        }
     }
-    return 0;
 }
 
-void bfs(int start,int V,vector<int>& ans,vector<vector<int>>& adj){
-    vector<bool> isVisited(V,false);
-    queue<int> q;
-    isVisited[start]=true;
-    q.push(start);
-    while (!q.empty()){
-        int node=q.front();
-        q.pop();
-        ans.push_back(node);
-        for(int item:adj[node]){
-            if(!isVisited[item]){
-                isVisited[item]=true;
-                q.push(item);
-            }    
-        }
+int main(){
+    int n, cost[20][20], i, j, visited[20], source;
+    cout <<"Enter the number of vertices:";
+    cin >>n;
+    cout <<"Enter the cost matrix (1 for edge & 0 for no-edge):\n";
+    for(i=1;i<=n;i++)
+        for (j=1;j<=n;j++)
+            cin >> cost[i][j];
+    for(i=1;i<=n;i++)
+        visited[i]=0;
+    cout <<"Enter the source node:";
+    cin >> source;
+    visited[source] = 1;
+    bfs(cost, source, visited, n);
+    for (i=1;i<=n;i++){
+        if (visited[i] != 0)
+            cout<<"\nNode " << i << " is reachable";
+        else
+            cout <<"\n Node " << i << " is not reachable";
     }
 }
 
